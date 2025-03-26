7d1a0a6b5016d8f94610298f4f9ac1e7a96190d1
jinsoftware <j4269631@gmail.com>
2025-03-26 23:36:39 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,19 +1,19 @@
 <?php
 $uploadDir = "uploads/";
 
-// Create folder if not exists
+// Create folder if it doesn't exist
 if (!file_exists($uploadDir)) {
     mkdir($uploadDir, 0777, true);
 }
 
-// Save text if available
+// Save text input (if available)
 if (!empty($_POST['text'])) {
     $textContent = $_POST['text'];
     $filename = $uploadDir . "text_" . date("Ymd_His") . ".txt";
     file_put_contents($filename, $textContent);
 }
 
-// Save uploaded files
+// Save uploaded files (if any)
 if (!empty($_FILES['files'])) {
     foreach ($_FILES['files']['tmp_name'] as $index => $tmpName) {
         $originalName = basename($_FILES['files']['name'][$index]);
@@ -22,6 +22,6 @@ if (!empty($_FILES['files'])) {
     }
 }
 
-// Return success response
+// Send success response
 echo json_encode(["status" => "success"]);
 ?>
